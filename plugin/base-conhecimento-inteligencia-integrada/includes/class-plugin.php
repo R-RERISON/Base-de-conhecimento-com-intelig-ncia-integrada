ab1361a098ed3586d1f1034a84984734c498767e
Ricardo Rerison <rickeris@gmail.com>
2026-09-14 21:17:18 -0300

    spec-002: registrar diagnostico temporario dev2

--- plugin/base-conhecimento-inteligencia-integrada/includes/class-plugin.php
+++ plugin/base-conhecimento-inteligencia-integrada/includes/class-plugin.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Registra apenas os hooks necessários aos vertical slices autorizados.
+ * Registra apenas os hooks necessários ao vertical slice autorizado.
  */
 final class Plugin {
 
@@ -23,5 +23,9 @@ final class Plugin {
 		add_action( 'admin_enqueue_scripts', array( Admin_Page::class, 'enqueue_assets' ) );
 		add_action( 'admin_post_' . Admin_Page::ACTION, array( Admin_Page::class, 'handle_save' ) );
 		add_action( 'admin_post_' . Classification_Admin::ACTION, array( Classification_Admin::class, 'handle_save' ) );
+
+		if ( defined( 'BDC_KB_HOMOLOGATION_BUILD' ) && BDC_KB_HOMOLOGATION_BUILD && class_exists( Classification_Diagnostics::class ) ) {
+			Classification_Diagnostics::register();
+		}
 	}
 }

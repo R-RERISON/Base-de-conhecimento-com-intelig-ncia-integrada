be12fb1207fc9c544b750d960252474cdbf24779
Ricardo Rerison <rickeris@gmail.com>
2026-09-15 12:31:06 -0300

    spec-003: remover runner concluido do runtime

--- plugin/base-conhecimento-inteligencia-integrada/includes/class-plugin.php
+++ plugin/base-conhecimento-inteligencia-integrada/includes/class-plugin.php
@@ -23,9 +23,5 @@ final class Plugin {
 		add_action( 'admin_enqueue_scripts', array( Admin_Page::class, 'enqueue_assets' ) );
 		add_action( 'admin_post_' . Admin_Page::ACTION, array( Admin_Page::class, 'handle_save' ) );
 		add_action( 'admin_post_' . Classification_Admin::ACTION, array( Classification_Admin::class, 'handle_save' ) );
-
-		if ( defined( 'BDC_KB_REVIEW_DIAGNOSTICS_BUILD' ) && BDC_KB_REVIEW_DIAGNOSTICS_BUILD ) {
-			Review_Diagnostics::register();
-		}
 	}
 }

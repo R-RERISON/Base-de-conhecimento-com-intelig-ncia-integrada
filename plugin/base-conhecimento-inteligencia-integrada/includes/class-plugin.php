10ba3dff24ece9dfb76c3937429c37ddefbd41c6
Ricardo Rerison <rickeris@gmail.com>
2026-09-14 21:04:07 -0300

    spec-002: registrar hooks da classificação

--- plugin/base-conhecimento-inteligencia-integrada/includes/class-plugin.php
+++ plugin/base-conhecimento-inteligencia-integrada/includes/class-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Orquestração mínima do runtime da SPEC-001.
+ * Orquestração mínima do runtime da Base de Conhecimento.
  *
  * @package BDC_Knowledge_Base
  */
@@ -12,14 +12,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Registra apenas os hooks necessários ao vertical slice autorizado.
+ * Registra apenas os hooks necessários aos vertical slices autorizados.
  */
 final class Plugin {
 
 	public static function register(): void {
 		add_action( 'init', array( Meta_Contract::class, 'register' ) );
+		add_action( 'init', array( Classification_Contract::class, 'register' ) );
 		add_action( 'admin_menu', array( Admin_Page::class, 'register_menu' ) );
 		add_action( 'admin_enqueue_scripts', array( Admin_Page::class, 'enqueue_assets' ) );
 		add_action( 'admin_post_' . Admin_Page::ACTION, array( Admin_Page::class, 'handle_save' ) );
+		add_action( 'admin_post_' . Classification_Admin::ACTION, array( Classification_Admin::class, 'handle_save' ) );
 	}
 }

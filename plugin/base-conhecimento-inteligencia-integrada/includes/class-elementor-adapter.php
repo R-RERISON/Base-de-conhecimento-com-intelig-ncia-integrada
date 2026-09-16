5f09a819d1e603a0fe2ec2cf3e1e3d9551ab5e30
Ricardo Rerison <rickeris@gmail.com>
2026-09-16 07:29:37 -0300

    spec-004: usar namespace estrutural canônico no Elementor

--- plugin/base-conhecimento-inteligencia-integrada/includes/class-elementor-adapter.php
+++ plugin/base-conhecimento-inteligencia-integrada/includes/class-elementor-adapter.php
@@ -101,7 +101,7 @@ final class Elementor_Adapter {
 	 */
 	private static function merge_result( array &$target, array $source ): void {
 		$namespace = 'elementor-merge-' . count( $target['fragments'] );
-		$source['fragments'] = Legacy_HTML_Adapter::namespace_fragments( $source['fragments'], $namespace );
+		$source['fragments'] = Content_Normalizer::namespace_structural_ids( $source['fragments'], $namespace );
 		foreach ( $source['fragments'] as $fragment ) {
 			$fragment['ordinal'] = count( $target['fragments'] );
 			$target['fragments'][] = $fragment;

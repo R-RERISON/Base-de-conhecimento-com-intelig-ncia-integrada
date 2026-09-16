3eb8e99923762015d95802bd65fbf439e7cc8ca2
Ricardo Rerison <rickeris@gmail.com>
2026-09-16 08:50:14 -0300

    spec-004: preparar build acceptance.7

--- plugin/base-conhecimento-inteligencia-integrada/base-conhecimento-inteligencia-integrada.php
+++ plugin/base-conhecimento-inteligencia-integrada/base-conhecimento-inteligencia-integrada.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Base de Conhecimento com Inteligência Integrada
  * Description: Base de Conhecimento com Summary narrativo, Classificação, Review & Governança, Content Extractor e Knowledge Document determinísticos.
- * Version: 0.4.0-acceptance.6
+ * Version: 0.4.0-acceptance.7
  * Requires at least: 6.6
  * Requires PHP: 8.1
  * Author: BDC
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'BDC_KB_VERSION', '0.4.0-acceptance.6' );
+define( 'BDC_KB_VERSION', '0.4.0-acceptance.7' );
 define( 'BDC_KB_SPEC004_PROFILE_BUILD', false );
 define( 'BDC_KB_SPEC004_G220_SMOKE_BUILD', false );
 define( 'BDC_KB_SPEC004_G230_SMOKE_BUILD', false );

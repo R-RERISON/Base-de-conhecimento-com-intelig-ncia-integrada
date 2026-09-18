d425b7d4e27d091eeaf627d6e395086a629ae3de
Ricardo Rerison <rickeris@gmail.com>
2026-09-18 17:09:30 -0300

    spec005: enable R-510 legacy Golden discovery build

--- plugin/base-conhecimento-inteligencia-integrada/base-conhecimento-inteligencia-integrada.php
+++ plugin/base-conhecimento-inteligencia-integrada/base-conhecimento-inteligencia-integrada.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Base de Conhecimento com Inteligência Integrada
  * Description: Base de Conhecimento com sumário, classificação, revisão, governança, estrutura editorial e recursos de inteligência integrados.
- * Version: 0.5.0-r500-t502.1
+ * Version: 0.5.0-r510-t510.1
  * Requires at least: 6.6
  * Requires PHP: 8.1
  * Author: BDC
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'BDC_KB_VERSION', '0.5.0-r500-t502.1' );
+define( 'BDC_KB_VERSION', '0.5.0-r510-t510.1' );
 define( 'BDC_KB_SPEC004_PROFILE_BUILD', false );
 define( 'BDC_KB_SPEC004_G220_SMOKE_BUILD', false );
 define( 'BDC_KB_SPEC004_G230_SMOKE_BUILD', false );
@@ -38,7 +38,8 @@ define( 'BDC_KB_SPEC004_G245_T100C_CORE_BLOCKS_ACTIVITY_BUILD', true );
 define( 'BDC_KB_SPEC004_G245_T100D_CORE_BLOCKS_EXECUTOR_BUILD', false );
 define( 'BDC_KB_SPEC004_T100E_E6_REGRESSION_BUILD', false );
 define( 'BDC_KB_SPEC004_G250_LIFECYCLE_BUILD', false );
-define( 'BDC_KB_SPEC005_R500_DIAGNOSTIC_BUILD', true );
+define( 'BDC_KB_SPEC005_R500_DIAGNOSTIC_BUILD', false );
+define( 'BDC_KB_SPEC005_R510_LEGACY_GOLDEN_BUILD', true );
 if ( ! defined( 'BDC_KB_ELEMENTOR_WRITER_ENABLED' ) ) {
 	define( 'BDC_KB_ELEMENTOR_WRITER_ENABLED', false );
 }
@@ -157,6 +158,9 @@ if ( defined( 'BDC_KB_SPEC004_G250_LIFECYCLE_BUILD' ) && BDC_KB_SPEC004_G250_LIF
 if ( defined( 'BDC_KB_SPEC005_R500_DIAGNOSTIC_BUILD' ) && BDC_KB_SPEC005_R500_DIAGNOSTIC_BUILD ) {
 	require_once BDC_KB_DIR . 'includes/class-search-baseline-diagnostic.php';
 }
+if ( defined( 'BDC_KB_SPEC005_R510_LEGACY_GOLDEN_BUILD' ) && BDC_KB_SPEC005_R510_LEGACY_GOLDEN_BUILD ) {
+	require_once BDC_KB_DIR . 'includes/class-legacy-golden-discovery.php';
+}
 
 \BDC\KnowledgeBase\Plugin::register();
 \BDC\KnowledgeBase\Visual_Foundation::register();
@@ -225,4 +229,7 @@ if ( defined( 'BDC_KB_SPEC004_G250_LIFECYCLE_BUILD' ) && BDC_KB_SPEC004_G250_LIF
 }
 if ( defined( 'BDC_KB_SPEC005_R500_DIAGNOSTIC_BUILD' ) && BDC_KB_SPEC005_R500_DIAGNOSTIC_BUILD ) {
 	\BDC\KnowledgeBase\Search_Baseline_Diagnostic::register();
+}
+if ( defined( 'BDC_KB_SPEC005_R510_LEGACY_GOLDEN_BUILD' ) && BDC_KB_SPEC005_R510_LEGACY_GOLDEN_BUILD ) {
+	\BDC\KnowledgeBase\Legacy_Golden_Discovery::register();
 }

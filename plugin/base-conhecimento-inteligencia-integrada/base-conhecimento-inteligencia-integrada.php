26dd47de0942a701c062af43c348fed5337fd23d
Ricardo Rerison <rickeris@gmail.com>
2026-09-19 09:02:00 -0300

    spec005: enable G550 Golden Gate build

--- plugin/base-conhecimento-inteligencia-integrada/base-conhecimento-inteligencia-integrada.php
+++ plugin/base-conhecimento-inteligencia-integrada/base-conhecimento-inteligencia-integrada.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Base de Conhecimento com Inteligência Integrada
  * Description: Base de Conhecimento com sumário, classificação, revisão, governança, estrutura editorial e recursos de inteligência integrados.
- * Version: 0.5.0-g540.1
+ * Version: 0.5.0-g550.1
  * Requires at least: 6.6
  * Requires PHP: 8.1
  * Author: BDC
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'BDC_KB_VERSION', '0.5.0-g540.1' );
+define( 'BDC_KB_VERSION', '0.5.0-g550.1' );
 define( 'BDC_KB_SPEC004_PROFILE_BUILD', false );
 define( 'BDC_KB_SPEC004_G220_SMOKE_BUILD', false );
 define( 'BDC_KB_SPEC004_G230_SMOKE_BUILD', false );
@@ -43,7 +43,8 @@ define( 'BDC_KB_SPEC005_R510_LEGACY_GOLDEN_BUILD', false );
 define( 'BDC_KB_SPEC005_R510_GOLDEN_BASELINE_BUILD', false );
 define( 'BDC_KB_SPEC005_R510_GOLDEN_AUTO_VALIDATOR_BUILD', false );
 define( 'BDC_KB_SPEC005_G530_SEARCH_ENGINE_BUILD', true );
-define( 'BDC_KB_SPEC005_G540_CORPUS_RUNNER_BUILD', true );
+define( 'BDC_KB_SPEC005_G540_CORPUS_RUNNER_BUILD', false );
+define( 'BDC_KB_SPEC005_G550_GOLDEN_RUNNER_BUILD', true );
 if ( ! defined( 'BDC_KB_ELEMENTOR_WRITER_ENABLED' ) ) {
 	define( 'BDC_KB_ELEMENTOR_WRITER_ENABLED', false );
 }
@@ -186,6 +187,10 @@ if ( defined( 'BDC_KB_SPEC005_G530_SEARCH_ENGINE_BUILD' ) && BDC_KB_SPEC005_G530
 if ( defined( 'BDC_KB_SPEC005_G540_CORPUS_RUNNER_BUILD' ) && BDC_KB_SPEC005_G540_CORPUS_RUNNER_BUILD ) {
 	require_once BDC_KB_DIR . 'includes/class-search-corpus-runner-g540.php';
 }
+if ( defined( 'BDC_KB_SPEC005_G550_GOLDEN_RUNNER_BUILD' ) && BDC_KB_SPEC005_G550_GOLDEN_RUNNER_BUILD ) {
+	require_once BDC_KB_DIR . 'includes/class-golden-suite-loader.php';
+	require_once BDC_KB_DIR . 'includes/class-golden-gate-runner-g550.php';
+}
 
 \BDC\KnowledgeBase\Plugin::register();
 \BDC\KnowledgeBase\Visual_Foundation::register();
@@ -266,4 +271,7 @@ if ( defined( 'BDC_KB_SPEC005_R510_GOLDEN_AUTO_VALIDATOR_BUILD' ) && BDC_KB_SPEC
 }
 if ( defined( 'BDC_KB_SPEC005_G540_CORPUS_RUNNER_BUILD' ) && BDC_KB_SPEC005_G540_CORPUS_RUNNER_BUILD ) {
 	\BDC\KnowledgeBase\Search_Corpus_Runner_G540::register();
+}
+if ( defined( 'BDC_KB_SPEC005_G550_GOLDEN_RUNNER_BUILD' ) && BDC_KB_SPEC005_G550_GOLDEN_RUNNER_BUILD ) {
+	\BDC\KnowledgeBase\Golden_Gate_Runner_G550::register();
 }

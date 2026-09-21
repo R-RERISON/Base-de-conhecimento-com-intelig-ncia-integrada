a29987b0d8ba84c68efc336c0af47a66b59d90c4
Ricardo Rerison <rickeris@gmail.com>
2026-09-21 10:22:09 -0300

    build(search): promote resumable G-590 candidate to rc2

--- tools/homologation/spec005/build-g590.py
+++ tools/homologation/spec005/build-g590.py
@@ -24,8 +24,8 @@ from pathlib import Path
 
 PLUGIN_REL = Path("plugin/base-conhecimento-inteligencia-integrada")
 BOOTSTRAP = "base-conhecimento-inteligencia-integrada.php"
-VERSION = "0.5.1-rc.1"
-BUILD_LABEL = "g590.1"
+VERSION = "0.5.1-rc.2"
+BUILD_LABEL = "g590.2"
 
 ENGINEERING_FLAGS_FALSE = (
     "BDC_KB_SPEC004_PROFILE_BUILD",
